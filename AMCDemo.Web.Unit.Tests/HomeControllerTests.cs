023b05eac88131f62bf57e219d05adffb54ec2c8
Ed Courtenay <ed@edcourtenay.co.uk>
2014-05-13 22:19:11 +0100

    Fix tests by adding IGreetingService mock objects

--- AMCDemo.Web.Unit.Tests/HomeControllerTests.cs
+++ AMCDemo.Web.Unit.Tests/HomeControllerTests.cs
@@ -1,7 +1,9 @@
 ﻿using System.Web.Mvc;
 using AMCDemo.Web.Controllers;
+using AMCDemo.Web.Interfaces;
 using AMCDemo.Web.ViewModels;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 
 namespace AMCDemo.Web.Unit.Tests
@@ -12,14 +14,16 @@ namespace AMCDemo.Web.Unit.Tests
         [Test]
         public void SutIsController()
         {
-            var sut = new HomeController();
+            var greetingService = Mock.Of<IGreetingService>();
+            var sut = new HomeController(greetingService);
             sut.Should().BeAssignableTo<Controller>();
         }
 
         [Test]
         public void IndexReturnsViewResult()
         {
-            var sut = new HomeController();
+            var greetingService = Mock.Of<IGreetingService>();
+            var sut = new HomeController(greetingService);
             var result = sut.Index();
             result.Should().BeOfType<ViewResult>();
         }
@@ -27,7 +31,8 @@ namespace AMCDemo.Web.Unit.Tests
         [Test]
         public void IndexReturnsViewResultForIndexView()
         {
-            var sut = new HomeController();
+            var greetingService = Mock.Of<IGreetingService>();
+            var sut = new HomeController(greetingService);
             var result = sut.Index();
             result.ViewName.Should().Be("Index");
         }
@@ -35,11 +40,10 @@ namespace AMCDemo.Web.Unit.Tests
         [Test]
         public void IndexReturnsViewResultWithModel()
         {
-            var sut = new HomeController();
+            var greetingService = Mock.Of<IGreetingService>();
+            var sut = new HomeController(greetingService);
             var result = sut.Index();
             result.Model.Should().BeOfType<HomeIndexViewModel>();
         }
-
-
     }
 }
