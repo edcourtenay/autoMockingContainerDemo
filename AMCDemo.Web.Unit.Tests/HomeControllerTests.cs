1f4d81e279ff0dd12cefecd39e3bacc76106e8d2
Ed Courtenay <ed@edcourtenay.co.uk>
2014-05-13 21:35:03 +0100

    Add SutIsController Test

--- AMCDemo.Web.Unit.Tests/HomeControllerTests.cs
+++ AMCDemo.Web.Unit.Tests/HomeControllerTests.cs
@@ -1,9 +1,18 @@
-﻿using NUnit.Framework;
+﻿using System.Web.Mvc;
+using AMCDemo.Web.Controllers;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace AMCDemo.Web.Unit.Tests
 {
     [TestFixture]
     public class HomeControllerTests
     {
+        [Test]
+        public void SutIsController()
+        {
+            var sut = new HomeController();
+            sut.Should().BeAssignableTo<Controller>();
+        }
     }
 }
---
+++ AMCDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace AMCDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
